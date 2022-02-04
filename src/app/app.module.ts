1743088f787d7b34502e6b2dabe2434fd3ff8e7f
sahana <sahnasbegam.mahaboobasha@citiustech.com>
2022-02-04 17:56:14 +0530

    Registration Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
 import { HeaderComponent } from './Shared/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HeaderComponent } from './Shared/header/header.component';
   imports: [
     BrowserModule,
     LoginModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
