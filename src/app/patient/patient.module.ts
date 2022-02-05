47276c54ec51981be9f4e20b4d10c99133af3f09
rajeshwari <rajeshwari.kg17@outlook.com>
2022-02-05 11:09:50 +0530

    Created Patient module and related components

---
+++ src/app/patient/patient.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PatientHeaderComponent } from './Components/patient-header/patient-header.component';
+import { DemographicComponent } from './Components/demographic/demographic.component';
+import { EmergencyContactComponent } from './Components/emergency-contact/emergency-contact.component';
+import { AllergyInfoComponent } from './Components/allergy-info/allergy-info.component';
+import { AppointmentComponent } from './Components/appointment/appointment.component';
+import { VitalSignsComponent } from './Components/vital-signs/vital-signs.component';
+import { ProcedureComponent } from './Components/procedure/procedure.component';
+import { DiagnosisComponent } from './Components/diagnosis/diagnosis.component';
+import { MedicationsComponent } from './Components/medications/medications.component';
+
+
+
+@NgModule({
+  declarations: [
+    PatientHeaderComponent,
+    DemographicComponent,
+    EmergencyContactComponent,
+    AllergyInfoComponent,
+    AppointmentComponent,
+    VitalSignsComponent,
+    ProcedureComponent,
+    DiagnosisComponent,
+    MedicationsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PatientModule { }
