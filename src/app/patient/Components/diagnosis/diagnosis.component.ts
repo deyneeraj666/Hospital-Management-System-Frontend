d71702991c2caa31518b4aaacff458721988fcc4
sahana <sahnasbegam.mahaboobasha@citiustech.com>
2022-02-09 13:07:59 +0530

    wednesdat 1st commit

--- src/app/patient/Components/diagnosis/diagnosis.component.ts
+++ src/app/patient/Components/diagnosis/diagnosis.component.ts
@@ -1,4 +1,6 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Diagnosis } from 'src/app/Models/Diagnosis';
 
 @Component({
   selector: 'app-diagnosis',
@@ -8,8 +10,27 @@ import { Component, OnInit } from '@angular/core';
 export class DiagnosisComponent implements OnInit {
   option:number=6;
   constructor() { }
-
+  public data:Diagnosis[]=[];
   ngOnInit(): void {
   }
-
+  public dcode:string="";
+  public dname:string="";
+  public ddate:Date=new Date();
+  public isDataNotfound:boolean=false;
+  
+  public btn_Add(){
+    let newData:Diagnosis=new Diagnosis();
+    newData.dcode=this.dcode;
+    newData.dname=this.dname;
+    newData.ddate=this.ddate;
+    //alert(this.ddate);
+    this.data.push(newData);
+    this.dcode="";
+    this.dname="";
+  }
+  public btn_Cancel(){
+    this.dcode="";
+    this.dname="";
+    //this.ddate=new Date();
+  }
 }
--- src/app/patient/Components/medications/medications.component.ts
+++ src/app/patient/Components/medications/medications.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Medications } from 'src/app/Models/Medications';
 
 @Component({
   selector: 'app-medications',
@@ -9,8 +10,44 @@ export class MedicationsComponent implements OnInit {
 
   option:number=8;
   constructor() { }
-
+  public data:Medications[]=[];
   ngOnInit(): void {
   }
-
+  public drug_name:string="";
+  public strength:string="";
+  public ddate:Date=new Date();
+  public frequency:string="";
+  public form:string="";
+  public quantity:string="";
+  public notes:string="";
+  add_medications(){
+    let newData:Medications=new Medications();
+    newData.strength=this.strength;
+    newData.frequency=this.frequency;
+    newData.drug_name=this.drug_name;
+    newData.notes=this.notes;
+    newData.form=this.form;
+    newData.quantity=this.quantity;
+    newData.ddate=this.ddate;
+    
+    this.data.push(newData);
+    this.strength="";
+    this.frequency="";
+    this.drug_name="";
+    this.notes="";
+    this.form="";
+    this.quantity="";
+  }
+  remove_medications(){
+    
+    this.strength="";
+    this.frequency="";
+    this.drug_name="";
+    this.notes="";
+    this.form="";
+    this.quantity="";
+    //this.ddate=new Date;
+    
+  }
+  
 }
